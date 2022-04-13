8129b039e5b16371d98b704c0c38ddd6a3216525
cngcruz <christopher.ng.cruz@gmail.com>
2022-04-12 21:26:58 -0700

    file upload functionality proof of concept

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from '../node_modules/react-bootstrap/Form';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <label>BAM!</label>
+        <div>
+        <Form.Group controlId="formFileMultiple" className="mb-3">
+          <Form.Control className="btn_file_select" type="file" multiple />
+        </Form.Group>
+        </div>
       </header>
     </div>
   );
