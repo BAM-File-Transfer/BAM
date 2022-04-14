182d81f3a30332f554df0ddc07b8611921ccf287
cngcruz <christopher.ng.cruz@gmail.com>
2022-04-14 16:34:41 -0700

    multiple file name population

--- client/src/Components/ChooseFiles.js
+++ client/src/Components/ChooseFiles.js
@@ -3,11 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const ChooseFiles = () => {
+    function handleFile(e) {
+        e.preventDefault();
+        console.log("here");
+        var list = document.getElementById('filelist');
+        var files = document.getElementById('files');
+        var userFiles = files.files;
+        console.log(userFiles);
+        list.innerHTML = '';
+        for (var i = 0; i < userFiles.length; i++) {
+            console.log(userFiles[i].name);
+          list.innerHTML += (i + 1) + '. ' + userFiles[i].name + '\n';
+        }
+        if (list.innerHTML == '') list.style.display = 'none';
+        else list.style.display = 'block';
+    };
 
     return (
-        <div className = "chooseFilesButtonCSS">
-            <input class="col-2 button chooseFilesButton" type="file" names="files[]" multiple></input>
-            <div className="fileList"></div>
+        <div className="chooseFile">
+            <input type="file" className="button" id="files" onChange={handleFile} multiple ></input>
+            <pre id="filelist"></pre>
         </div>
     );
 }
