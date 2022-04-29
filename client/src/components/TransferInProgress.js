d6341ab5ed3ca4cf93d41aedca199267bed9339e
Abdullah Kose <akose@ucsc.edu>
2022-04-28 17:55:13 -0700

    routing is implemented

--- client/src/components/TransferInProgress.js
+++ client/src/components/TransferInProgress.js
@@ -3,22 +3,27 @@ import Header from "./Header"
 import SuperheroName from "./SuperheroName"
 import '../styles/transferinprogress.css'
 import '../styles/button.css'
-
+import { useNavigate } from 'react-router-dom'
 
 const TransferInProgress = () => {
+  const navigate = useNavigate()
   return (
     <div className="App">
         <Header />
         <SuperheroName />
         <div className="transfertext">
-          <button className = "button">SEND</button>
-          <button className = "button">RECEIVE</button>
+          <button className = "button" onClick={() => {
+              navigate('/SenderSuccess')
+          }}>SEND</button>
+          <button className = "button" onClick={() => {
+              navigate('/ReceiverSuccess')
+          }}>RECEIVE</button>
 
           <div className="text-style">
           Transfer In Progress
         </div>
         </div>
-        
+
     </div>
   )
 }
