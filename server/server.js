57592242ec8cb17578acc85c351bd5e192a8bfc4
Eric Truong <truongmeric@gmail.com>
2022-05-05 03:53:56 +0000

    [Experimenting] I figured out how to get POST and GET requests.
    Currently doesn't really do anything. I'll need to have it fetch from the database.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const PORT = 5000;
 
 app.get('/api', (req, res) => {
   res.json({ testObject: ['testElement'] })
 })
 
-app.listen(5000, () => { console.log('Server started on port 5000') })
+app.post('/send', (req, res) => {
+  res.json({ status: 'OK' })
+})
+
+app.get('/recv', (req, res) => {
+  res.json({ time: Date.now() })
+})
+
+app.listen(PORT, () => { console.log('Server started on 129.146.60.126:%d', PORT) })
