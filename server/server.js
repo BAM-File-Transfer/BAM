f518c5007d49b82d9e52587c7e9869f88437b5f2
Brandon Minjares <brandon.minjares@gmail.com>
2022-05-06 02:02:44 -0700

    Connect app to MongoDB from dev mode

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('../config/db')
+
+dotenv.config({ path: './../config/config.env' })
+
+connectDB()
+
 const app = express()
 
 app.get('/api', (req, res) => {
   res.json({ testObject: ['testElement'] })
 })
 
-app.listen(5000, () => { console.log('Server started on port 5000') })
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => { console.log('Server started on port 5000') })
