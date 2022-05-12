7047a1f0c5c214cd43631d9bb17ff4b71063996a
Abdullah Kose <akose@ucsc.edu>
2022-05-12 12:28:34 -0700

    In previous commit program was getting the rotation data, now it takes the actual accelerometer data in 3 axis.

--- client/src/components/WaitForBump.js
+++ client/src/components/WaitForBump.js
@@ -18,18 +18,16 @@ const WaitForBump = () => {
     // Start by checking if the device is mobile
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
       document.getElementById("bam-image").addEventListener('click', () => {
-        //console.log("clicked ")
         DeviceMotionEvent.requestPermission().then(response => {
           if (response == 'granted') {
             console.log("accelerometer permission granted")
-            window.addEventListener('deviceorientation',(event) => {
-                // Expose each orientation angle in a more readable way
+            window.addEventListener("devicemotion", (event) => {
                 // This will be refreshed too frequent if the refresh rate isn't defined
-                let rotation_degrees = event.alpha;
-                let frontToBack_degrees = event.beta;
-                let leftToRight_degrees = event.gamma;
+                let x_acceleration = event.acceleration.x;
+                let y_acceleration = event.acceleration.y;
+                let z_acceleration = event.acceleration.z;
                 // Print the acc sensor data to console
-                console.log("Acc data: " + rotation_degrees + " " + frontToBack_degrees + " " + leftToRight_degrees)
+                console.log("X Acc: " + x_acceleration + "Y Acc: " + y_acceleration + "Z Acc: " + z_acceleration)
             })
           }
         })
