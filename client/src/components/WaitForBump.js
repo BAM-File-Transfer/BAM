b5baabd2567d65b14b5646c8d3b571d8f7246555
Abdullah Kose <akose@ucsc.edu>
2022-05-12 10:57:51 -0700

    Successfuly asked for permission to use accelerometer data and printed to the console. See the client/src/components/WaitForBump.js code and comments.

--- client/src/components/WaitForBump.js
+++ client/src/components/WaitForBump.js
@@ -9,17 +9,58 @@ import FistsBumping from '../assets/FistsBumping.png'
 
 const WaitForBump = () => {
   const navigate = useNavigate()
+  // Accelerometer event listener
+  React.useEffect(() => {
+    // If the bam image is clicked, we will ask for permission to get acc data
+    // We can't ask this automatically, the user has to trigger the permission
+    // event with a click or something else
+    // The connection has to be HTTPS
+    // Start by checking if the device is mobile
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+      document.getElementById("bam-image").addEventListener('click', () => {
+        //console.log("clicked ")
+        DeviceMotionEvent.requestPermission().then(response => {
+          if (response == 'granted') {
+            console.log("accelerometer permission granted")
+            window.addEventListener('deviceorientation',(event) => {
+                // Expose each orientation angle in a more readable way
+                // This will be refreshed too frequent if the refresh rate isn't defined
+                let rotation_degrees = event.alpha;
+                let frontToBack_degrees = event.beta;
+                let leftToRight_degrees = event.gamma;
+                // Print the acc sensor data to console
+                console.log("Acc data: " + rotation_degrees + " " + frontToBack_degrees + " " + leftToRight_degrees)
+            })
+          }
+        })
+      })
+    } else {
+      alert("This is a desktop device, unable to access acc sensors")
+    }
+  }, []);
+  /*
+  React.useEffect(() => {
+    // check if the client is on mobile device
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+      alert("this is a mobile device")
+    }
+  }, []);
+  */
+
   return (
     <div className="App">
         <Header />
         <SuperheroName />
+        {/*
+        Sender and receiver buttons for routing
         <button className = "test-button" onClick={() => {
             navigate('/Progress')
         }}>Sender</button>
         <button className = "test-button" onClick={() => {
             navigate('/Progress')
         }}>Receiver</button>
-        <div className='fists-bumping-container'>
+        */}
+        <div id = "bam-image" className='fists-bumping-container'>
                 <img src = {FistsBumping} className="fists-bumping-image-size" alt="Fist Bump Waiting Pic"/>
                 <h1 className="text-style">
                     <br/>
@@ -28,6 +69,7 @@ const WaitForBump = () => {
                     TO INITIATE TRANSFER!
                 </h1>
         </div>
+
         <button className = "red-button-bottom" onClick={() => {
             navigate('/')
         }}>CANCEL</button>
